fe28f4b48e20be2e4da6daf7f98d2e53ed14239f
GW <garykachunwu@gmail.com>
2024-06-21 22:11:39 -0700

    finished js files for models

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought} = require ('../models');
+
+module.exports = {
+  async getUsers(req,res) {
+    try{
+      const users = await User.find();
+      res.json(users);
+    } catch (err){
+      res.status(500).json(err.message);
+    }
+  },
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId });
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+}
---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema,Types } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username:{
+      type:String,
+      required:true,
+      trim:true,
+      unique:true
+    },
+    email:{
+      type:String,
+      required:true,
+      unique:true,
+      match: /\w+@\w+\.\w{3}/,
+    },
+    thoughts:[
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends:[
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+)
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+model.exports = User;
